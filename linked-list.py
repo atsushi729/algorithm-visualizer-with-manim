21cd57660b7ba53f28bc0fdf5ef494eee1f3ae41
Atsushi <atu729@icloud.com>
2024-05-20 23:16:52 +0900

    fix linked-list.py

--- linked-list.py
+++ linked-list.py
@@ -3,66 +3,92 @@ from manim import *
 
 class LinkedListVisualization(Scene):
     def construct(self):
-        title = Text("Linked List Visualization", font_size=24)
+        title = Text("Linked List Operations Visualization").scale(0.9)
         self.play(Write(title))
         self.wait(1)
         self.play(FadeOut(title))
 
-        linked_list = []
-        texts = []
-        arrows = []
-        action_text = Text('', font_size=36).to_corner(UP + RIGHT)
+        # Initial elements in the linked list
+        elements = [5, 3, 8, 1, 4]
+        linked_list = VGroup()  # Group for linked list elements
 
-        def create_node(text, color=WHITE):
-            node = Rectangle(width=2, height=1, color=color)
-            txt = Text(text, color=BLACK, font_size=32).move_to(node.get_center())
-            return node, txt
+        # Generate linked list nodes with numbers inside
+        for i, elem in enumerate(elements):
+            node = Square(side_length=1).set_stroke(color=WHITE, width=3)
+            num = Text(str(elem), color=WHITE).move_to(node.get_center())
+            node_group = VGroup(node, num)
 
-        def create_arrow(from_node, to_node):
-            return Arrow(from_node.get_right(), to_node.get_left(), buff=0.1)
+            # Create arrow if not the last element
+            if i < len(elements) - 1:
+                arrow = Arrow(start=node.get_right(), end=node.get_right() + RIGHT * 0.8, buff=0.1, stroke_width=2)
+                node_group.add(arrow)
 
-        # Function to visualize adding an element to the linked list
-        def add_element(element):
-            new_action_text = Text(f'Add({element})', font_size=36).to_corner(UP + RIGHT)
-            self.play(Transform(action_text, new_action_text))
-            node, txt = create_node(element)
-            if linked_list:
-                node.next_to(linked_list[-1], RIGHT, buff=1)
-                arrow = create_arrow(linked_list[-1], node)
-                arrows.append(arrow)
-                self.play(Create(arrow))
-            else:
-                node.move_to(ORIGIN)
-            txt.move_to(node.get_center())
-            linked_list.append(node)
-            texts.append(txt)
-            self.play(Create(node), Write(txt))
+            linked_list.add(node_group)
 
-        # Function to visualize removing an element from the linked list
-        def remove_element():
-            if linked_list:
-                node = linked_list.pop()
-                txt = texts.pop()
-                if arrows:
-                    arrow = arrows.pop()
-                    self.play(FadeOut(arrow))
-                new_action_text = Text(f'Remove({txt.text})', font_size=36).to_corner(UP + RIGHT)
-                self.play(Transform(action_text, new_action_text))
-                self.play(FadeOut(node), FadeOut(txt))
+        # Position the linked list nodes in a row
+        linked_list.arrange(RIGHT, buff=0.1)
+        linked_list.to_edge(UP)
 
-        # Add and remove elements from the linked list with operation text
-        add_element("5")
+        # Animate creation of the linked list nodes
+        self.play(Create(linked_list))
         self.wait(1)
-        add_element("3")
+
+        # Highlight the head of the linked list
+        head_highlight = SurroundingRectangle(linked_list[0], color=BLUE, buff=0.1)
+        head_text = Text("Head", color=BLUE).scale(0.7).next_to(linked_list[0], UP)
+        self.play(Create(head_highlight), Write(head_text))
         self.wait(1)
-        add_element("8")
-        self.wait(1)
-        remove_element()
-        self.wait(1)
-        add_element("2")
-        self.wait(1)
-        remove_element()
+
+        # Adding an element to the linked list
+        new_element = 6
+        new_node = Square(side_length=1).set_stroke(color=WHITE, width=3)
+        new_num = Text(str(new_element), color=WHITE).move_to(new_node.get_center())
+        new_arrow = Arrow(start=new_node.get_left(), end=new_node.get_left() - LEFT * 0.8, buff=0.1, stroke_width=2)
+        new_group = VGroup(new_node, new_num, new_arrow)
+        new_group.next_to(linked_list, RIGHT, buff=0.1)
+
+        self.play(Write(Text("Adding 6 to the linked list").next_to(linked_list, DOWN, buff=0.5)))
+        self.play(AnimationGroup(Create(new_node), Write(new_num), Create(new_arrow), lag_ratio=0.5))
+        linked_list.add(new_group)
         self.wait(2)
+
+        # Update the tail highlight
+        tail_highlight = SurroundingRectangle(new_group, color=GREEN, buff=0.1)
+        tail_text = Text("Tail", color=GREEN).scale(0.7).next_to(new_group, UP)
+        self.play(Create(tail_highlight), Write(tail_text))
+        self.wait(1)
+
+        # Deleting an element from the linked list (element 8)
+        delete_index = 2
+        delete_highlight = SurroundingRectangle(linked_list[delete_index], color=RED, buff=0.1)
+
+        self.play(Write(Text("Deleting 8 from the linked list").next_to(linked_list, DOWN, buff=1.5)))
+        self.play(Create(delete_highlight))
+        self.wait(1)
+        self.play(FadeOut(linked_list[delete_index]), FadeOut(delete_highlight))
+
+        # Update arrows
+        if delete_index > 0:
+            previous_node = linked_list[delete_index - 1]
+            next_node = linked_list[delete_index + 1]
+            new_arrow = Arrow(start=previous_node[0].get_right(), end=next_node[0].get_left(), buff=0.1, stroke_width=2)
+            self.play(Create(new_arrow))
+            previous_node.add(new_arrow)
+
+        linked_list.remove(linked_list[delete_index])
+
+        # Rearrange nodes after deletion and ensure new element is correctly positioned
+        self.play(linked_list.animate.arrange(RIGHT, buff=0.1).to_edge(UP))
+
+        # Move highlights together with nodes
+        self.play(head_highlight.animate.move_to(linked_list[0]), head_text.animate.next_to(linked_list[0], UP))
+        self.play(tail_highlight.animate.move_to(linked_list[-1]), tail_text.animate.next_to(linked_list[-1], UP))
+
+        self.wait(2)
+
+        # Cleanup
+        self.play(FadeOut(linked_list), FadeOut(head_highlight), FadeOut(head_text), FadeOut(tail_highlight),
+                  FadeOut(tail_text))
 
 # To run the scene, use the following command in your terminal:
 # manim -pql linked_list_visualization.py LinkedListVisualization
